80506dd07a16c9a14d389777e95b68c4c0345ff3
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2023-08-21 20:09:01 +0700

    Update jackson naming strategy

--- infrastructure/src/main/java/org/example/request/ExpenseRequest.java
+++ infrastructure/src/main/java/org/example/request/ExpenseRequest.java
@@ -1,19 +1,19 @@
 package org.example.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ExpenseRequest {
 
     private Integer id;
     private String name;
-    @JsonProperty("account_number")
     private Long accountNumber;
     private String type;
-    @JsonProperty("is_distributed")
     private Boolean isDistributed;
     private Integer status;
 }
--- infrastructure/src/main/java/org/example/request/ExpenseSearchRequest.java
+++ infrastructure/src/main/java/org/example/request/ExpenseSearchRequest.java
@@ -1,13 +1,14 @@
 package org.example.request;
 
 import org.example.dto.PageRequest;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ExpenseSearchRequest extends PageRequest {
 
     private String name = "";
-    @JsonProperty("account_number")
     private String accountNumber = "";
 }
